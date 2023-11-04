f78f00a16e7ce06eeeaa97d8ba492a5861914c74
ChristinaGaitan <christina.gaitan@gmail.com>
2023-11-04 12:11:44 -0600

    quite los alerts de tipos de datos

--- ejerciciosTiposDatos.js
+++ ejerciciosTiposDatos.js
@@ -4,20 +4,10 @@
 const nombre = 'María';
 console.log('Nombre: ', nombre);
 
-// No utilice console porque que fuera una alerta
-let nombreUsuario = prompt('¿Cómo te llamas?');
-alert('Nombre de usuario: ' + nombreUsuario);
-
 // 2. Números
 const edad = 20;
 console.log('Edad: ', edad);
 
-let edadUsuario = prompt('¿Cuántos años tienes?');
-alert('Edad de usuario: ' + edadUsuario);
-
 // 3. Booleanos
 const esEstudiante = false;
 console.log('Estudiante: ', esEstudiante);
-
-let usuarioEsEstudiante = confirm('¿Eres estudiante?');
-alert('Usuario estudiante: ' + usuarioEsEstudiante);
